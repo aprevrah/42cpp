8ba7d5e5e2901c9ab8a509a97b5c0dec03fee208
aprevrha <aprevrha@gmail.com>
2024-12-01 19:45:56 +0100

    fix(0/ex00): now works w/o algorithm and with c++98

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 
 int main(int argc, char **argv) {
     if (argc <= 1)
         return std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0;
     for (int i = 1; i < argc; ++i) {
             std::string arg = argv[i];
-            std::transform(arg.begin(), arg.end(), arg.begin(), ::toupper);
-            std::cout << arg;
-            if (i < argc - 1)
-                std::cout << " ";
+            for (int j = 0; arg[j]; ++j)
+            {
+                std::cout << (char)toupper(arg[j]);
+            }
         }
         std::cout << std::endl;
     return 0;
