7580ca618482017d4a7b5a6393aae9a44f47f842
aprevrha <aprevrha@gmail.com>
2024-12-02 15:28:12 +0100

    feat(1/ex00): Zombie

---
+++ CPP_01/ex00/Zombie.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Zombie {
+	public:
+		Zombie( std::string name );
+		~Zombie( void );
+
+		void announce( void );
+	private:
+	std::string _name;
+};
+
+Zombie*	newZombie( std::string name );
+void	randomChump( std::string name );
