b1be45c1326c99ff9c463b358cbfcf570c51af13
aprevrha <aprevrha@gmail.com>
2025-06-20 13:47:41 +0200

    fix(9/ex01): error code

--- CPP_09/ex01/main.cpp
+++ CPP_09/ex01/main.cpp
@@ -5,9 +5,11 @@
 #include "RPN.hpp"
 
 int main(int argc, char **argv) {
-    if (argc != 2)
+    if (argc != 2) {
         std::cerr << "Expected one arg" << std::endl;
-
+        return 1;
+    }
+        
     std::string expr = argv[1];
     RPN RPN(expr);
     int result = 0;
@@ -17,9 +19,11 @@ int main(int argc, char **argv) {
     }
     catch (const std::invalid_argument &e) {
         std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
     }
     catch (const std::exception &e) {
         std::cerr << "Unexpected error: " << e.what() << std::endl;
+        return 1;
     }
     
     return 0;
